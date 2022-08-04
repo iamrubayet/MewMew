cf7be040f071cbdc21b9cd36d69287fa77fec964
rubayet170746 <rafirubayet75@gmail.com>
2022-08-05 03:49:51 +0600

    first commit

---
+++ client/client.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');//grabbing an element
+const loadingElement = document.querySelector('.loading');
+const API_URL = 'http://localhost:5000/mews';
+
+loadingElement.style.display = 'none';
+
+form.addEventListener('submit',(event)=>{
+	event.preventDefault();
+	const formData = new FormData(form);
+	const name = formData.get('name');
+	const content = formData.get('content');
+
+	const mew = {
+		name,
+		content
+
+	};
+
+form.style.display = 'none';
+loadingElement.style.display = '';
+
+fetch(API_URL,{
+	method: 'POST',
+	body : JSON.stringify(mew),
+	headers : {
+		'content-type': 'application/json'
+	}
+});
+
+
+
+});
