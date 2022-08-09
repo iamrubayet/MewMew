27479d5c9e70c69cabd36750d19941f3a8d5a1ac
rubayet170746 <rafirubayet75@gmail.com>
2022-08-09 14:00:49 +0600

    validated the user input

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function isValidMew(mew){
+	return mew.name && mew.name.toString().trim() !== '' &&
+	      mew.content && mew.content.toString().trim() !== '';
+}
+
 
 
 app.get('/',(req,res)=>{
@@ -17,7 +22,25 @@ app.get('/',(req,res)=>{
 });
 
 app.post('/mews',(req,res)=>{
-	console.log(req.body);
+	if(isValidMew(req.body))
+	{
+		const mew = {
+		name : req.body.name.toString(),
+		content : req.body.content()
+
+	};
+	console.log(mew);
+
+	}
+	else
+	{
+		res.status(422);
+		res.json({
+			message: 'hey name and content are required'
+
+		})
+
+	}
 
 });
 
